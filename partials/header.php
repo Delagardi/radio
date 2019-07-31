f920a27a57ab71822f09b8e1aa2b8ec0ea3394cc
Alek Arbuzov <arbuzov.alek@gmail.com>
2019-07-31 16:55:18 +0200

    Added desktop header

--- partials/header.php
+++ partials/header.php
@@ -1,8 +1,83 @@
 <div class="header">
-    <div class="ui inverted segment">
-        <div class="ui inverted secondary pointing menu">
-            <a class="active item">Home</a>
-            <a class="item">World</a>
+        <div class="ui menu top-menu borderless">
+            <div class="ui container">
+                <div class="ui label red item">CHANNEL</div>
+                <div class="ui dropdown icon item">
+                    <img src="<?= url('images/header/hq_logo.svg') ?>">
+                    <div class="text">Radio 3 Shuttle</div>
+                    <i class="dropdown icon"></i>
+                </div>
+                <div class="right menu">
+                    <div class="item date">
+                        <i class="ui clock icon"></i>
+                        Saturday, 22 December 2018 - 10:30 PM UTC+12
+                    </div>
+                    <div class="ui button item">
+                        <i class="ui sign in icon"></i>
+                        Sign In
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="navigation">
+            <div class="ui container">
+                <div class="ui two column stackable grid status">
+                    <div class="column logo">
+                        <img src="<?= url('images/header/site_logo.svg') ?>" alt="logo" class="logo">
+                    </div>
+                    <div class="column live">
+                        <div class="ui horizontal basic segments">
+                            <div class="ui segment live-status">
+                                <div class="items">
+                                    <div class="item">
+                                        <div class="ui label small red">LIVE</div>
+                                    </div>
+                                    <div class="item">
+                                        <div class="ui label black time">14:00-15:30</div>
+                                    </div>
+                                </div>
+                                <div class="group-logo">
+                                    <img src="<?= url('images/header/group.png') ?>">
+                                </div>
+                            </div>
+                            <div class="ui segment">
+                                <div class="ui buttons">
+                                    <div class="ui tiny button">On Air</div>
+                                    <div class="ui basic tiny button">Coming Up</div>
+                                </div>
+                                <div class="song-info">
+                                    <h5>Pure American Country Radio Show</h5>
+                                    <div class="artist">Stanley Fisher</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="ui borderless inverted menu horizontal-nav">
+                    <div class="item active">Home</div>
+                    <div class="ui dropdown item">World<i class="dropdown icon"></i></div>
+                    <div class="ui dropdown item">Sports<i class="dropdown icon"></i></div>
+                    <div class="ui dropdown item">Tech<i class="dropdown icon"></i></div>
+                    <div class="ui dropdown item">Community<i class="dropdown icon"></i></div>
+                    <div class="ui dropdown item">Shop<i class="dropdown icon"></i></div>
+                    <div class="ui dropdown item">Features<i class="dropdown icon"></i></div>
+                    <div class="item">
+                        <span class="rectangle"></span>
+                        <span class="rectangle"></span>
+                        <span class="rectangle"></span>
+                    </div>
+                    <div class="right menu">
+                        <div class="ui buttons">
+                            <button class="ui primary labeled icon button">
+                                <i class="play icon"></i>
+                                <div class="text">Listen Now</div>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
